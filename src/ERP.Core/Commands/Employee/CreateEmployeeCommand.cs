34157fec31b18f2332a5c4e03d47946fc01dde17
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-06-24 16:50:25 +0430

    Added FluentValidatior

--- src/ERP.Core/Commands/Employee/CreateEmployeeCommand.cs
+++ src/ERP.Core/Commands/Employee/CreateEmployeeCommand.cs
@@ -1,7 +1,10 @@
-﻿using MediatR;
+﻿using FluentValidation;
+
+using MediatR;
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +13,77 @@ namespace ERP.Core.Commands.Employee;
 
 public class CreateEmployeeCommand : IRequest<long>
 {
+    [DisplayName("نام")]
     public string FirstName { get; set; }
+    [DisplayName("نام خانوادگی")]
     public string LastName { get; set; }
+    [DisplayName("کد پرسنلی")]
     public int EmpoloyeeNo { get; set; } = 0;
+    [DisplayName("نام پدر")]
     public string FatherName { get; set; }
+    [DisplayName("کد ملی")]
     public string NationalCode { get; set; }
+    [DisplayName("شماره شناسنامه")]
     public string IdentifyNo { get; set; }
+    [DisplayName("تاریخ تولد")]
     public string DateOfBirth { get; set; }
+    [DisplayName("جنسیت")]
     public short Gender { get; set; }
+    [DisplayName("تاریخ استخدام")]
     public string HireDate { get; set; }
+    [DisplayName("تاریخ ترک کار")]
     public string LeaveDate { get; set; } = "";
+    [DisplayName("شماره موبایل")]
     public string MobileNo { get; set; }
+    [DisplayName("مسیرعکس")]
     public string ImaghePath { get; set; } = "";
-} 
+}
+
+
+public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+{
+    public CreateEmployeeCommandValidator()
+    {
+
+        RuleFor(v => v.FirstName)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.")
+            .MaximumLength(50).WithMessage("{PropertyName} نباید بیشتر از {MaxLength} کاراکتر باشد.")
+            .MinimumLength(3).WithMessage("{PropertyName} حداقل {MinLength} کاراکتر باشد.");
+
+        RuleFor(v => v.LastName)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.")
+            .MaximumLength(50).WithMessage("{PropertyName} نباید بیشتر از {MaxLength} کاراکتر باشد.")
+            .MinimumLength(3).WithMessage("{PropertyName} حداقل {MinLength} کاراکتر باشد.");
+
+        RuleFor(v => v.EmpoloyeeNo).Equal(0).WithMessage("{PropertyName} را وارد نمایید.");
+
+        RuleFor(v => v.FatherName)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.")
+            .MaximumLength(50).WithMessage("{PropertyName} نباید بیشتر از {MaxLength} کاراکتر باشد.")
+            .MinimumLength(3).WithMessage("{PropertyName} حداقل {MinLength} کاراکتر باشد.");
+              
+        RuleFor(v => v.NationalCode)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.")
+            .MaximumLength(10).WithMessage("{PropertyName} نباید بیشتر از {MaxLength} کاراکتر باشد.")
+            .MinimumLength(10).WithMessage("{PropertyName} حداقل {MinLength} کاراکتر باشد.");
+                       
+        RuleFor(v => v.IdentifyNo)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.")
+            .MinimumLength(1).WithMessage("{PropertyName} حداقل {MinLength} کاراکتر باشد.");
+
+        RuleFor(v => v.DateOfBirth)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.")
+            .MaximumLength(10).WithMessage("{PropertyName} نباید بیشتر از {MaxLength} کاراکتر باشد.")
+            .MinimumLength(10).WithMessage("{PropertyName} حداقل {MinLength} کاراکتر باشد.");
+                          
+        RuleFor(v => v.DateOfBirth)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.")
+            .MaximumLength(10).WithMessage("{PropertyName} نباید بیشتر از {MaxLength} کاراکتر باشد.")
+            .MinimumLength(10).WithMessage("{PropertyName} حداقل {MinLength} کاراکتر باشد.");
+                                  
+        RuleFor(v => v.MobileNo)
+            .NotEmpty().WithMessage("{PropertyName} را وارد نمایید.");
+        
+    }
+
+}
--- src/ERP.Core/Interfaces/ICommandHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using ERP.Domain.DTOs.Exceptions;
-
-namespace ERP.Core.Interfaces;
-
-public interface ICommandHandler<in T>  
-{
-    Result Handle(T command);
-}
