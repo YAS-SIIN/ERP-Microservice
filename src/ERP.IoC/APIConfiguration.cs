90f7ab2e953a4fd50031f0865b2413604fbd1a54
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-07-10 14:14:03 +0330

    Fixed bugs

--- src/ERP.IoC/APIConfiguration.cs
+++ src/ERP.IoC/APIConfiguration.cs
@@ -1,9 +1,9 @@
-﻿using ERP.Domain.Interfaces.UnitOfWork;
-using ERP.Infra.Data.CoreContext;
-using ERP.Infra.Data.UnitOfWork;
+﻿//using ERP.Domain.Interfaces.UnitOfWork;
+//using ERP.Infra.Data.CoreContext;
+//using ERP.Infra.Data.UnitOfWork;
 
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using System.Reflection;
@@ -12,16 +12,16 @@ namespace ERP.IoC;
 
 public static class APIConfiguration
 {
-    public static void Register(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddDbContext<MAIN_ERPDBContext>(options => options.UseSqlServer(configuration["ApplicationOptions:MAIN_ERPConnectionString"]));
-        services.AddDbContext<READ_ERPDBContext>(options => options.UseSqlServer(configuration["ApplicationOptions:READ_ERPConnectionString"]));
-        services.AddDbContext<WRITE_ERPDBContext>(options => options.UseSqlServer(configuration["ApplicationOptions:WRITE_ERPConnectionString"]));
-        //builder.Services.AddDbContext<MyDataBase>(options => options.UseInMemoryDatabase("MyDB"));           
-        //services.AddMediatR(Assembly.GetExecutingAssembly());
-        //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
-        services.AddScoped<CoreDBContextInjection>();
-    }
+    //public static void Register(this IServiceCollection services, IConfiguration configuration)
+    //{
+    //    //services.AddDbContext<MAIN_ERPDBContext>(options => options.UseSqlServer(configuration["ApplicationOptions:MAIN_ERPConnectionString"]));
+    //    //services.AddDbContext<READ_ERPDBContext>(options => options.UseSqlServer(configuration["ApplicationOptions:READ_ERPConnectionString"]));
+    //    //services.AddDbContext<WRITE_ERPDBContext>(options => options.UseSqlServer(configuration["ApplicationOptions:WRITE_ERPConnectionString"]));
+    //    ////builder.Services.AddDbContext<MyDataBase>(options => options.UseInMemoryDatabase("MyDB"));           
+    //    ////services.AddMediatR(Assembly.GetExecutingAssembly());
+    //    ////services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+    //    //services.AddScoped<IUnitOfWork, UnitOfWork>();
+    //    //services.AddScoped<CoreDBContextInjection>();
+    //}
 
 }
